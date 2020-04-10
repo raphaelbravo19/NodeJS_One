80cbb1ca752dacdb2d16223286d3d3f275e6b2ed
Raphael Bravo <raphael.bravo@eigital.com>
2020-04-10 00:15:43 -0400

    port80

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const morgan = require("morgan");
 
 //setting
-app.set("port", process.env.PORT || 80)
+app.set("port", process.env.PORT || 3000)
 app.set("json spaces",2 )
 
 //middlewares
