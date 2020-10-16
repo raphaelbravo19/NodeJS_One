d432fd72e274edd4fdfc4506ee699e8fcc6ee458
Raphael Bravo <57974596+TechMaagal@users.noreply.github.com>
2020-10-16 16:40:59 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-const express =  require("express")
+const express = require("express")
 const app = express();
 const morgan = require("morgan");
 
 //setting
-app.set("port", process.env.PORT || 3000)
-app.set("json spaces",2 )
+app.set("port", process.env.PORT || 80)
+app.set("json spaces", 2)
 
 //middlewares
 app.use(morgan("dev"))
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 //routes
 app.use(require("./routes/index"))
-app.use("/api/pokemon",require("./routes/pokemon"))
+app.use("/api/pokemon", require("./routes/pokemon"))
 // starting server
-app.listen(app.get("port"),()=>{
+app.listen(app.get("port"), () => {
     console.log(`Server on port ${app.get("port")}`)
 })
